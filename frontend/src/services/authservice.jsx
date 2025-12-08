3bc9666e2ea7a94ce18beb33403ae3885e88d51e
kripa1205 <kripepper12@gmail.com>
2025-12-08 14:21:24 +0530

    Frontend + backend full project added

--- frontend/src/services/authservice.jsx
+++ frontend/src/services/authservice.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/users";
+const API = `${import.meta.env.VITE_API_URL}/api/auth`;
 
 export const registerUser = async (data) => {
   return await axios.post(`${API}/register`, data);
